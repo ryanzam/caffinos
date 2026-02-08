87a2bfd3979596afcb8a1aab3e14a0f4e706f465
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-02-08 11:31:55 +0545

    project setup and add header components

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Index = () => {
+    return (
+        <div className='min-h-screen'>
+            <Header />
+        </div>
+    )
+}
+
+export default Index
